314013f670d9295dbb5d48d996e443dc0ef8ec1a
Rava <rava@copen.local>
2015-05-09 12:55:45 +0900

    framework

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+# require 'spec_helper'
+
+# describe "StaticPages" do
+#   describe "GET /static_pages" do
+#     it "works! (now write some real specs)" do
+#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#       get static_pages_index_path
+#       response.status.should be(200)
+#     end
+#   end
+# end
+
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App  | Contact")
+    end
+  end
+end
